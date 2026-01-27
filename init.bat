78f07ea9f96c46f634e3c4d6f9345099544c2249
Qking521 <qking88521@gmail.com>
2026-01-27 14:58:27 +0800

    解决init.bat脚本echo输出污染for语句的问题

--- init.bat
+++ init.bat
@@ -1,5 +1,6 @@
 @echo off
 setlocal enabledelayedexpansion
+set "DEBUG=0"
 :: *******************获取当前日期和时间，格式：MMDD-HHMM**********************
 for /f "tokens=2-4 delims=/.- " %%a in ("%date%") do (
     set MM=%%b
@@ -12,39 +13,37 @@ for /f "tokens=1-2 delims=: " %%a in ("%time%") do (
 
 :: 去掉前导空格
 if "%HH:~0,1%"==" " set HH=0%HH%
-set format_time=%MM%%DD%-%HH%%MN%
-echo formated time = %format_time%
+set FORMAT_TIME=%MM%%DD%-%HH%%MN%
 :: *******************获取当前日期和时间，格式：MMDD-HHMM**********************
 
 :: *******************获取当前脚本所在目录**********************
 :: --结尾有反斜杠
 set "SCRIPT_DIR=%~dp0"
-echo SCRIPT_DIR=%SCRIPT_DIR%
 if "%1"=="" (
 	set INIT_BAT=!SCRIPT_DIR!init.bat
-	echo basepath=!INIT_BAT!
-
 	set ABD_CHECK_BAT=!SCRIPT_DIR!adb_check.bat
-	echo adbpath=!ABD_CHECK_BAT!
-	
 	set BASE_OUT_DIR=!SCRIPT_DIR!OUT\
-	echo base_Out=!BASE_OUT_DIR!
-	
 )else (
 	set "SCRIPT_DIR=%1"
-	echo script_dir = !SCRIPT_DIR!
-	
 	for %%a in ("!SCRIPT_DIR:~0,-1!") do set "LAST_DIR=%%~nxa"
 	set OUT_DIR=!BASE_OUT_DIR!!LAST_DIR!
-	echo out_dir=!OUT_DIR!
+)
+if "%DEBUG%"=="1" (
+	echo FORMAT_TIME=%FORMAT_TIME%
+	echo SCRIPT_DIR=%SCRIPT_DIR%
+	echo INIT_BAT=%INIT_BAT%
+	echo ABD_CHECK_BAT=%ABD_CHECK_BAT%
+	echo BASE_OUT_DIR=%BASE_OUT_DIR%
+	echo OUT_DIR=%OUT_DIR%
 )
 endlocal & (
-	set format_time=%format_time%
+	set FORMAT_TIME=%FORMAT_TIME%
 	set SCRIPT_DIR=%SCRIPT_DIR%
 	set INIT_BAT=%INIT_BAT%
 	set ABD_CHECK_BAT=%ABD_CHECK_BAT%
 	set BASE_OUT_DIR=%BASE_OUT_DIR%
 	set OUT_DIR=%OUT_DIR%
+	
 )
 :: *******************获取当前脚本所在目录**********************
 
